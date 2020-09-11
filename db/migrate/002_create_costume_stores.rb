e43a16e7c8f5413a22d9ac951c2949a90ab524f8
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-11 23:33:37 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,8 @@
 # Create your costume_stores migration here
+<<<<<<< HEAD
 
+=======
+>>>>>>> 3c29b4705ac6fbfcb72592aab686b2d44696733e
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
@@ -14,4 +17,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.timestamps null: false
     end
   end
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> 3c29b4705ac6fbfcb72592aab686b2d44696733e
